35689881971271d463bad5becb70ba8bac76ba4c
DaniGarcia02 <155740693+DaniGarcia02@users.noreply.github.com>
2024-01-06 02:12:04 +0100

    KFold and LeaveOneOut Working

--- src/main.py
+++ src/main.py
@@ -8,28 +8,29 @@ from ucimlrepo import fetch_ucirepo
 
  
 class Node():
-    def __init__(self, attribute, prevDecision, children=None):
+    def __init__(self, attribute, prevDecision, nSamples, children=None):
         self.attribute = attribute
         self.prevDecision = prevDecision
+        self.nSamples = nSamples
         
         if children is None:
             children = []
         self.children = children
         
 
-def VisualizeTree(node, i, file):
+def VisualizeTree(node, file, i=0):
     if i == 0:
         string = ""
     else:
         string = "  |" + (i-1) * "              |"
-        string += "-- " + str(node.prevDecision) + " -->"
+        string += "-- " + str(node.prevDecision) + " --> "
     
     if node.children != []:
-        file.write(string + "{" + node.attribute + "}" + "\n")    
+        file.write(string + "{" + node.attribute + "} " + str(node.nSamples) + " \n")    
         for child in node.children:
-            VisualizeTree(child, i+1, file)
+            VisualizeTree(child, file, i+1)
     else:
-        file.write(string + "{" + node.attribute + "}" + "\n")
+        file.write(string + "{" + node.attribute + "} " + str(node.nSamples) + " \n")
     
         
 def GetValueRepetitions(column):
@@ -55,8 +56,7 @@ def Entropy(column):
     return -entropy
 
 
-def EntropySA(df, attribute):
-    values = GetValueRepetitions(df[attribute])
+def EntropySA(df, attribute, values):
     entropySA = 0
     
     for key in values:
@@ -65,18 +65,16 @@ def EntropySA(df, attribute):
     return entropySA
 
 
-def SplitInfo(column):
-    values = GetValueRepetitions(column)
-    splitInfo = 0
+def SplitInfo(column, values):
+    splitInfo = math.pow(1, -5)
     
     for key in values:
-        splitInfo += values[key] / len(column) * math.log(values[key] / len(column), 2)
+        splitInfo -= values[key] / len(column) * math.log(values[key] / len(column), 2)
     
-    return -splitInfo    
+    return splitInfo    
 
 
-def Gini(df, attribute):    
-    values = GetValueRepetitions(df[attribute])
+def Gini(df, attribute, values):    
     giniIndex = 0
     
     for key in values:
@@ -95,9 +93,9 @@ def MajorityVote(column):
     values = GetValueRepetitions(column)
         
     if values['+'] >= values['-']:
-        return 'M+'
+        return '+'
     else:
-        return 'M-'
+        return '-'
 
     
 def RemoveRows(df, attribute, value):
@@ -133,28 +131,31 @@ def allEqual(l, symbol):
 def ID3(df, candidates, method=0, decision="Root"):   
     
     if allEqual(df['A16'], '+'):
-        return Node('+', decision)
+        return Node('+', decision, len(df))
             
     if allEqual(df['A16'], '-'):
-        return Node('-', decision)
+        return Node('-', decision, len(df))
     
     if not candidates:
-        return Node(MajorityVote(df['A16']), decision)
+        return Node(MajorityVote(df['A16']), decision, len(df))
     
     classifier = None
 
-    # Gain or Gain-Ratio
+    # Gain (ID3) or Gain-Ratio (C4.5)
     if method != 2:
         bestGain = 0
         entropy = Entropy(df['A16'])
         for attribute in candidates:
-            entropySA = EntropySA(df, attribute)
+            values = GetValueRepetitions(df[attribute])
+            
+            entropySA = EntropySA(df, attribute, values)
             gain = entropy - entropySA
             
-            # Gain-Ratio
+            # Gain-Ratio (C4.5)
             if method == 1:
-                splitInfo = SplitInfo(df[attribute])
+                splitInfo = SplitInfo(df[attribute], values)
                 gain = gain / splitInfo
+                
                 
             if gain > bestGain:
                 bestGain = gain
@@ -164,16 +165,18 @@ def ID3(df, candidates, method=0, decision="Root"):
     if method == 2:
         bestGini = 1
         for attribute in candidates:
-            gini = Gini(df, attribute)        
+            values = GetValueRepetitions(df[attribute])
+            
+            gini = Gini(df, attribute, values)        
             if gini < bestGini:
                 bestGini = gini
                 classifier = attribute
    
     if classifier == None:
-        return Node(MajorityVote(df['A16']), decision)
+        return Node(MajorityVote(df['A16']), decision, len(df))
     
-    node = Node(classifier, decision)
-    
+    node = Node(classifier, decision, len(df))
+
     values = GetValueRepetitions(df[classifier])
 
     for key in values:
@@ -186,31 +189,183 @@ def ID3(df, candidates, method=0, decision="Root"):
     return node 
     
 
+def PredictRec(node, row):
+    if node.children == []:
+        return node.attribute
+    
+    for child in node.children:
+        if row[node.attribute] == child.prevDecision:
+            return PredictRec(child, row)
+    
+    return None
+
+
+def Predict(root, test):
+    
+    confusionMatrix = {'TP': 0, 'TN': 0, 'FP': 0, 'FN': 0}
+    unableToPredict = 0
+    
+    for i, row in test.iterrows():
+        solution = PredictRec(root, row)
+        
+        if solution == '+' and solution == row['A16']:
+            confusionMatrix['TP'] += 1
+        elif solution == '+' and solution != row['A16']:
+            confusionMatrix['FP'] += 1
+        elif solution == '-' and solution == row['A16']:
+            confusionMatrix['TN'] += 1
+        elif solution == '-' and solution != row['A16']:
+            confusionMatrix['FN'] += 1
+        else:  
+            unableToPredict += 1    
+            
+    return confusionMatrix, unableToPredict
+
+
+def KFold(df, candidates, k, method=0):
+    
+    folds = np.array_split(df, k)
+    bestAccuracy = 0
+    total = 0
+    bestRoot = None
+    
+    for i, df_i in enumerate(folds):
+        test = df_i
+        emptyTrain = True
+        for j, df_j in enumerate(folds):
+            if i == j:
+                continue            
+            if emptyTrain:
+                train = df_j
+                emptyTrain = False
+            else:
+                train = pd.concat([train, df_j])
+                
+        print("===== Building Tree", i , "=====")        
+                
+        st = time.time()
+        root = ID3(train, candidates, method)
+        et = time.time()
+        
+        print("Time:", et - st,)
+        
+        print("===== Making Predicition ", i, "=====")  
+        
+        st = time.time()
+        confusionMatrix, unableToPredict = Predict(root, test)
+        et = time.time()
+        
+        print('-- Confusion Matrix --')
+        print(confusionMatrix)
+        
+        print('Test Length:', len(test))
+        print('Unable to Predict:', unableToPredict)
+        
+        accuracy = (confusionMatrix['TP'] + confusionMatrix['TN']) / len(test)
+        print("Accuracy:",  accuracy * 100, "%")
+        
+        
+        if confusionMatrix['TP'] + confusionMatrix['FP'] != 0:
+            precision = confusionMatrix['TP'] / (confusionMatrix['TP'] + confusionMatrix['FP'])
+        else:
+            precision = 0
+        print("Precision:", precision * 100, "%")
+            
+        if confusionMatrix['TP'] + confusionMatrix['FN'] != 0:
+            sensitivity = confusionMatrix['TP'] / (confusionMatrix['TP'] + confusionMatrix['FN'])
+        else:
+            sensitivity = 0
+        print("Sensitivity:", sensitivity * 100, "%")
+        
+        if confusionMatrix['FP'] != 0 or confusionMatrix['TN']:
+            specificity = confusionMatrix['TN'] / (confusionMatrix['FP'] + confusionMatrix['TN'])
+        else:
+            specificity = 0
+        print("Specificity:", specificity * 100, "%")
+        
+        print("Time:", et - st, '\n')
+        
+        total += accuracy
+        
+        if bestAccuracy < accuracy:
+            bestAccuracy = accuracy
+            bestRoot = root
+    
+    meanAccuracy = total / k
+    
+    if bestRoot == None:
+        bestRoot = root
+        bestAccuracy = accuracy
+     
+    return bestRoot, meanAccuracy
+
+
+def LeaveOneOut(df, candidates, method=0):
+    
+    bestAccuracy = 0
+    total = 0
+    bestRoot = None
+    
+    i = 0
+    while i != df.index[-1]:
+        while i not in df.index:
+            i += 1
+        
+        test = df.loc[[i]]
+        train = copy.deepcopy(df)
+        train.drop(i, inplace=True)
+        
+        print("===== Building Tree", i , "=====")        
+                
+        root = ID3(train, candidates, method)
+        
+        confusionMatrix, unableToPredict = Predict(root, test)
+        
+        accuracy = (confusionMatrix['TP'] + confusionMatrix['TN']) / len(test)
+        total += accuracy
+        
+        if bestAccuracy < accuracy:
+            bestAccuracy = accuracy
+            bestRoot = root
+            
+        i += 1
+    
+    meanAccuracy = total / len(df)
+    
+    if bestRoot == None:
+        bestRoot = root
+        bestAccuracy = accuracy
+         
+    return bestRoot, meanAccuracy
+
+
 def main():
     df, candidates = readDataset()
     
-    print(df)
+    method = 1
     
-    method = 0
-    
-    st = time.time()
-    root = ID3(df, candidates, method)
-    et = time.time()
-    
-    #print(root)
-
     if method == 2:
-        filename = 'GiniTree.txt'
+        filename = '../TreeRepresentation/Gini_Tree.txt'
+        string = 'Gini'
     elif method == 1:
-        filename = 'GainRatioTree.txt'
+        filename = '../TreeRepresentation/C4.5_Tree.txt'
+        string = 'Gain Ratio (C4.5)'
     else:
-        filename = 'GainTree.txt' 
+        filename = '../TreeRepresentation/ID3_Tree.txt'
+        string = 'Gain (ID3)'
     
+    print("Method selected:", string, "\n")
+    
+    #root, meanAccuracy = KFold(df, candidates, 10, method)
+    root, meanAccuracy = LeaveOneOut(df, candidates, method)
+    
+    print("Saving tree with best accuracy...")
     with open(filename, 'w') as file:
-        VisualizeTree(root, 0, file)
-    
-    print("Time:", et - st)
-    
+        VisualizeTree(root, file)
+        
+    print("Tree saved in:  " + filename + "\n") 
+        
+    print("Mean of the accuracy of all trees: ", meanAccuracy * 100, "%")
     
     
 if __name__=="__main__":
